fe9a03b96c40024975ca3af9f358329f122584c2
iamibie <iamahamane@gmail.com>
2022-10-03 20:12:46 -0500

    new updates

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = { 
     apps: [{
-      name    : "index.js",
-      script  : "node",
+      name    : "nodejs-vanillaJS-App",
+      script  : "npm",
       args    : "start"
     }]
   }
