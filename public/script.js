5c723e12dbe9eeeecfd8be5d1dd9dd553d3bc249
iamibie <iamahamane@gmail.com>
2024-07-10 23:30:13 -0500

    jul1024

--- public/script.js
+++ public/script.js
@@ -2,67 +2,35 @@ const hamburger = document.getElementById("h-menu")
 const nav = document.getElementById("nav")
 const closeIcon = document.querySelector('.close')
 const openIcon = document.querySelector('.open')
+const hi = document.getElementById('hi')
+const ispan = document.getElementById('i-span')
+const img = document.querySelectorAll('.img')
 
 
 
-const toggleMenu = ()=> {
-    if (nav.classList.contains("nav-show")) {
-      nav.classList.remove("nav-show");
-     
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 0) {
+      nav.classList.add('sticky');
     } else {
-      nav.classList.add("nav-show");
-
+      nav.classList.remove('sticky');
     }
-  }
-  
-  hamburger.addEventListener("click", toggleMenu);
+  });
 
 
+window.addEventListener('scroll', ()=>{
+    const triggerBottom = window.innerHeight / 5 * 4
 
-
-
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-window.addEventListener('load', ()=>{
-    const fName = document.querySelector('.im')
-    const sofDev = document.querySelector('.sd')
-    const cybEng = document.querySelector('.ce')
-    
-    fName.classList.remove('im')
-    sofDev.classList.remove('sd')
-    cybEng.classList.remove('ce')
-
-   
-
-
-
+    img.forEach(img => {
+        const rect = img.getBoundingClientRect().top
+        if(rect < triggerBottom) {
+            img.classList.add('img-animation')
+        }else{
+            img.classList.remove('img-animation')
+        }
+    })
 
 })
-
-
-window.addEventListener("scroll", ()=>{
-    const scrollPosition = window.pageYOffset / 10 
-    const navBar = document.querySelector('.navBar')
-
-    
-
-
-    const navBarTop = navBar.getBoundingClientRect().top
-
-    /*console.log(scrollPosition)
-    console.log(navBarTop)*/
-
-    
-    if(navBarTop < scrollPosition) {
-        navBar.classList.add('nav-active')
-    }else{
-        navBar.classList.remove('nav-active')
-    }
-    
-   
-    
-})
-
 
 window.addEventListener('scroll', () => {
     const boxes = document.querySelectorAll('.box')
@@ -82,6 +50,137 @@ window.addEventListener('scroll', () => {
 })
 
 
+
+
+   /*
+window.addEventListener("scroll", ()=>{
+     
+    const triggerBottom = window.innerHeight / 5 * 8
+
+    const hiTop = hi.getBoundingClientRect().top
+
+    if(hiTop < triggerBottom) {
+        hi.classList.add('animate__animated', 'animate__bounceInLeft')
+    }else{
+        hi.classList.remove('animate__animated', 'animate__bounceInLeft')
+    }
+    
+ 
+    const scrollPosition = window.pageYOffset / 10
+    
+    if(navBarTop < scrollPosition) {
+        navBar.classList.add('nav-active')
+    }else{
+        navBar.classList.remove('nav-active')
+    }
+   
+    
+})
+ */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/**
+ * const toggleMenu = ()=> {
+    if (nav.classList.contains("nav-show")) {
+      nav.classList.remove("nav-show");
+     
+    } else {
+      nav.classList.add("nav-show");
+
+    }
+  }
+  
+  hamburger.addEventListener("click", toggleMenu);
+ */
+
+
+
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+/**
+ * window.addEventListener("scroll", ()=>{
+    const scrollPosition = window.pageYOffset / 10 
+    const navBar = document.querySelector('.navBar')
+
+    
+
+
+    const navBarTop = navBar.getBoundingClientRect().top
+
+    console.log(scrollPosition)
+    console.log(navBarTop)
+
+    
+    if(navBarTop < scrollPosition) {
+        navBar.classList.add('nav-active')
+    }else{
+        navBar.classList.remove('nav-active')
+    }
+    
+   
+    
+})
+
+
+
+
+
+ */
 
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
